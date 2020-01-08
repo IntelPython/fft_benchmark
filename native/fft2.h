336e952364350548d4293e4e0f36ec43f10f019f
Sameer Bibikar <sameer.bibikar@intel.com>
2020-01-08 15:53:24 -0600

    move FFT benchmarks to main

--- native/fft2.h
+++
@@ -1,8 +0,0 @@
-#include "fft_bench.h"
-
-#ifndef _FFT2_H
-#define _FFT2_H
-
-double *fft2(const struct bench_options opts);
-
-#endif
--- native/fft2_cdp-in-c.h
+++
@@ -1,8 +0,0 @@
-#include "fft_bench.h"
-
-#ifndef _FFT2_CDP_IN_C_H
-#define _FFT2_CDP_IN_C_H
-
-double *fft2_cdp_in_c(const struct bench_options *opts);
-
-#endif
