a95389f2c73110ff622545694b5fcdf4b5a0968e
Sameer Bibikar <sameer.bibikar@intel.com>
2020-01-13 14:11:33 -0600

    add quick getopt impl for windows

--- native/win_compile_all.bat
+++ native/win_compile_all.bat
@@ -1,7 +1,7 @@
 @ECHO off
 
-SET _MKL_DIR=C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2017.2.187\windows\mkl
+SET _MKL_DIR=C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\mkl
 
-icl /c /nologo /O3 /MD /W3 /Qstd=c99 /fp:strict /QxSSE4.2 /QaxCORE-AVX2,COMMON-AVX512 /Qopenmp -I "%_MKL_DIR%\include" /D_CRT_SECURE_NO_WARNINGS /DNDEBUG fft_bench.c
-icl /nologo fft_bench.obj /Fe"build/fft_bench.exe" /link /LIBPATH:"%_MKL_DIR%\lib\intel64_win" mkl_rt.lib
+icl /c /nologo /O3 /MD /W3 /Qstd=c99 /fp:strict /QxSSE4.2 /QaxCORE-AVX2,COMMON-AVX512 /Qopenmp -I"%_MKL_DIR%\include" /D_CRT_SECURE_NO_WARNINGS /DNDEBUG /Qmkl fft_bench.c
+icl /nologo fft_bench.obj /Fe"fft_bench.exe" /link /LIBPATH:"%_MKL_DIR%\lib\intel64_win" mkl_rt.lib
 del fft_bench.obj
